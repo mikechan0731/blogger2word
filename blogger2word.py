f003d0c512aa097683aee297e7e00c64eba572fc
MikeChan <m7807031@gmail.com>
2019-08-27 10:44:50 +0800

    first commit.

---
+++ blogger2word.py
@@ -0,0 +1,32 @@
+# -*-coding: utf-8-*-
+# editor: MikeChan
+# email: m7807031@gmail.com
+# license: BSD
+
+# OS import
+import os
+import random
+import sqlite3
+from datetime import date
+
+# Parser import
+import requests
+from urllib import request
+from bs4 import BeautifulSoup
+
+# ===== Page Structure =====#
+
+
+# ===== Global Variables ===== #
+
+
+# ====== functions ===== #
+
+
+# ====== main() ===== #
+def main():
+    print("Done!")
+
+
+if __name__=="__main__":
+    main()
